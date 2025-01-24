891f86f6e23a0c143cb03b64f4bb77e74a071e1d
Roman.Iastremskyi <iastremskyi010397@gmail.com>
2025-01-24 20:11:16 +0300

    added functionality for registration and authorization

--- config/settings.py
+++ config/settings.py
@@ -155,3 +155,7 @@ LOGGING = {
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "users.CustomUser"
+
+LOGIN_REDIRECT_URL = "mailing:home"
+
+LOGIN_URL = "users:login"
--- config/urls.py
+++ config/urls.py
@@ -6,6 +6,7 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("mailing/", include("mailing.urls", namespace="mailing")),
+    path("users/", include("users.urls", namespace="users")),
 ]
 
 if settings.DEBUG:
