4a9df6ead119bf31dd05d36ffbb2f0171899cbd5
Roman.Iastremskyi <iastremskyi010397@gmail.com>
2025-01-28 20:44:44 +0300

    changed the logic of send_email command

--- mailing/management/commands/send_mail.py
+++ mailing/management/commands/send_mail.py
@@ -7,48 +7,49 @@ from mailing.models import MailingAttempt, MailingUnit, Message
 
 class Command(BaseCommand):
     help = """This command sends emails to all receivers in the mailing. It takes the title of the message as an argument.
-    Example: python manage.py send_mail "Title of the message" """
+        You can also specify a list of recipient email addresses using the --recipients argument.
+        Example: python manage.py send_mail "Title of the message" --recipients user1@example.com user2@example.com"""
 
     def add_arguments(self, parser):
         parser.add_argument("title", type=str, help="Title of the message to send")
+        parser.add_argument(
+            "--recipients",
+            nargs="+",
+            type=str,
+            help="List of recipient email addresses",
+        )
+        parser.add_argument(
+            "--message",
+            nargs="+",
+            type=str,
+            help="Text of the message to send",
+        )
 
     def handle(self, *args, **kwargs):
         title = kwargs["title"]
-        message = Message.objects.get(title=title)
-        mailing = MailingUnit.objects.get(message_id=message.id)
-        self.__send_emails(mailing)
-        self.__update_status(mailing, "Launched")
+        message = kwargs.get("message")
+        recipients = kwargs.get("recipients")
+        self.__send_emails(title, message, recipients)
 
-        self.stdout.write(self.style.SUCCESS("Mailing was launched successfully"))
 
-    def __send_emails(self, mailing_unit):
-        recipients = [receiver.email for receiver in mailing_unit.receivers.all()]
+    def __send_emails(self, title, message, recipients=None):
+        if recipients is None:
+            self.stdout.write(self.style.ERROR("You didn't specify recipients email"))
         for receiver in recipients:
             try:
                 send_mail(
-                    subject=mailing_unit.message.title,
-                    message=mailing_unit.message.body,
+                    subject=title,
+                    message=message,
                     from_email=settings.DEFAULT_FROM_EMAIL,
                     recipient_list=[receiver],
                     fail_silently=False,
                 )
             except BadHeaderError:
-                return self.__handle_exception("Invalid header found.", receiver, mailing_unit)
+                return self.__handle_exception("Invalid header found.", receiver, title)
             except Exception as e:
-                return self.__handle_exception(str(e), receiver, mailing_unit)
+                return self.__handle_exception(str(e), receiver, title)
             else:
-                MailingAttempt.objects.create(
-                    mailing=mailing_unit, status="Success", server_answer=f"Email успешно отправлен для {receiver}"
-                )
+                self.stdout.write(self.style.SUCCESS(f"Message '{title}' to '{receiver}' sent successfully"))
 
-    def __handle_exception(self, error_message, receiver, mailing_unit):
-        MailingAttempt.objects.create(
-            mailing=mailing_unit,
-            status="Failed",
-            server_answer=f'Возникла ошибка: "{error_message}" при отправке на {receiver}',
-        )
-
-    def __update_status(self, mailing_unit, status):
-        if mailing_unit.status != status:
-            mailing_unit.status = status
-            mailing_unit.save()
+    def __handle_exception(self, error_message, receiver, title):
+        self.stdout.write(self.style.ERROR(f"Message '{title}' sent to '{receiver}' failed: {error_message}"))
